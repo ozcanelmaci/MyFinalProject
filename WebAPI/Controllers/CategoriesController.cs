6d482e304b1fa419a852660e7ba855158338e611
Özcan Elmacı <ozcan.elmaci@ogr.deu.edu.tr>
2021-03-14 00:12:59 +0300

    13 Mart 2021 18.ders sonu

---
+++ WebAPI/Controllers/CategoriesController.cs
@@ -0,0 +1,33 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            var result = _categoryService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);//istersek result ın hepsini yazdırırız, istersek de yalnız data veya mesaj yazdırırız.
+            }
+            return BadRequest(result);
+        }
+    }
+}
